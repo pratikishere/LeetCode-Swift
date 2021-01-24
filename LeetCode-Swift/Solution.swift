20f950471aa3c6d85e2ffe5142056f0682493593
pratikishere <pbpatel1990@yahoo.com>
2021-01-24 12:35:26 +0530

    Move Solution to production target

---
+++ LeetCode-Swift/Solution.swift
@@ -0,0 +1,33 @@
+//
+//  Solution.swift
+//  LeetCode-Swift
+//
+//  Created by Pratik Patel on 24/01/21.
+//
+
+import Foundation
+
+public struct Solution {
+
+    public init() {}
+
+    public func getFirstUniqueCharacterIndex(from givenString: String) -> Int {
+        var dic = [Character: Bool]()
+
+        for char in givenString {
+            if let _ = dic[char] {
+                dic[char] = true
+            } else {
+                dic[char] = false
+            }
+        }
+
+        for (i, char) in givenString.enumerated() {
+            if dic[char] == false {
+                return i
+            }
+        }
+
+        return -1
+    }
+}
--- LeetCode-SwiftTests/FirstUniqueCharacterInStringTests.swift
+++ LeetCode-SwiftTests/FirstUniqueCharacterInStringTests.swift
@@ -6,28 +6,7 @@
 //
 
 import XCTest
-
-struct Solution {
-    func getFirstUniqueCharacterIndex(from givenString: String) -> Int {
-        var dic = [Character: Bool]()
-
-        for char in givenString {
-            if let _ = dic[char] {
-                dic[char] = true
-            } else {
-                dic[char] = false
-            }
-        }
-
-        for (i, char) in givenString.enumerated() {
-            if dic[char] == false {
-                return i
-            }
-        }
-
-        return -1
-    }
-}
+import LeetCode_Swift
 
 class FirstUniqueCharacterInStringTests: XCTestCase {
 
